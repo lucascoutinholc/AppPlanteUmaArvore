38c957f7d9fea40d4a923fc2a6a53e8aabc1514a
Lucas Coutinho <lukascoutinhoalmeida@gmail.com>
2021-01-24 18:51:27 -0300

    Add layout for third screen and image of map

---
+++ app/src/main/java/com/example/appplanteumaarvore/TerceiraActivity.java
@@ -0,0 +1,14 @@
+package com.example.appplanteumaarvore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TerceiraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terceira);
+    }
+}
