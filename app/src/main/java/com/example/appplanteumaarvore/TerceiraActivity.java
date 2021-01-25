f1ace19632db0c02d2589e4a0171b15ca30b8b1f
Lucas Coutinho <lukascoutinhoalmeida@gmail.com>
2021-01-24 21:27:20 -0300

    Add method for change activity 3 to activity 4

--- app/src/main/java/com/example/appplanteumaarvore/TerceiraActivity.java
+++ app/src/main/java/com/example/appplanteumaarvore/TerceiraActivity.java
@@ -2,7 +2,9 @@ package com.example.appplanteumaarvore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TerceiraActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class TerceiraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terceira);
+    }
+
+    public void verRanking(View view) {
+        Intent intent = new Intent(this, QuartaActivity.class);
+        startActivity(intent);
     }
 }
