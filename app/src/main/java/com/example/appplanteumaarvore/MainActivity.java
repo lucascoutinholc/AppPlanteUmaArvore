a81d40ea0350dac4104ccf8f7d3b2c9abf9627b1
Lucas Coutinho <lukascoutinhoalmeida@gmail.com>
2021-01-27 23:03:37 -0300

    Add id for EditTexts in first layout. On MainActivity.java at method entrar was added the Room connection and was verified if the user have account on app

--- app/src/main/java/com/example/appplanteumaarvore/MainActivity.java
+++ app/src/main/java/com/example/appplanteumaarvore/MainActivity.java
@@ -1,22 +1,53 @@
 package com.example.appplanteumaarvore;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextNome, editTextEmail, editTextSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextNome = findViewById(R.id.textoNome);
+        editTextEmail = findViewById(R.id.textoEmail);
+        editTextSenha = findViewById(R.id.textoSenha);
     }
 
     public void entrar(View view) {
-        Intent intent = new Intent(this, SegundaActivity.class);
-        startActivity(intent);
+        String nomeDigitado = editTextNome.getText().toString();
+        String emailDigitado = editTextEmail.getText().toString();
+        String senhaDigitado = editTextSenha.getText().toString();
+
+        BancoUsuario bu = Room.databaseBuilder(this, BancoUsuario.class,
+                "bd-app").allowMainThreadQueries().build();
+
+        UsuarioDao uDao = bu.criarBanco();
+        if (uDao.verificarUsuario(nomeDigitado, emailDigitado, senhaDigitado) != null) {
+            Toast.makeText(this, "Bem vindo, " + nomeDigitado, Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(this, SegundaActivity.class);
+            startActivity(intent);
+
+            bu.close();
+        } else {
+            Toast.makeText(this, "Usuário não cadastrado! " +
+                    "Se cadastre no link acima!", Toast.LENGTH_SHORT).show();
+
+            bu.close();
+
+            editTextNome.setText("");
+            editTextEmail.setText("");
+            editTextSenha.setText("");
+        }
     }
 
     public void abrirCadastro(View view) {
