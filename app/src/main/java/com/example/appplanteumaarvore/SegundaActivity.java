e47e0e7ae7ce51f50f1db875dbb0cc16978bdae4
Lucas Coutinho <lukascoutinhoalmeida@gmail.com>
2021-01-28 16:09:12 -0300

    Add id for CheckBoxs in second layout. On SegundaActivity.java at method comprar was added String for show to user your selection

--- app/src/main/java/com/example/appplanteumaarvore/SegundaActivity.java
+++ app/src/main/java/com/example/appplanteumaarvore/SegundaActivity.java
@@ -5,16 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class SegundaActivity extends AppCompatActivity {
+
+    CheckBox checkBoxJacaranda, checkBoxIpe, checkBoxQuaresmeira;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        checkBoxJacaranda = findViewById(R.id.checkJacaranda);
+        checkBoxIpe = findViewById(R.id.checkIpe);
+        checkBoxQuaresmeira = findViewById(R.id.checkQuaresmeira);
     }
 
     public void comprar(View view) {
+        String sementesEscolhidas = "";
+
+        if (checkBoxJacaranda.isChecked()) {
+            sementesEscolhidas += " Jacarandá. ";
+        }
+        if (checkBoxIpe.isChecked()) {
+            sementesEscolhidas += " Ipê Amarelo. ";
+        }
+        if (checkBoxQuaresmeira.isChecked()) {
+            sementesEscolhidas += " Quaresmeira. ";
+        }
+
+        Toast.makeText(this, "A(s) semente(s) escolhida(s) foi/foram:" + sementesEscolhidas,
+                Toast.LENGTH_LONG).show();
+
         Intent intent = new Intent(this, TerceiraActivity.class);
         startActivity(intent);
     }
