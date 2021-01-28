c0e7b074179b317ee2f679946387216f5171d29b
Lucas Coutinho <lukascoutinhoalmeida@gmail.com>
2021-01-27 23:10:17 -0300

    Add id for EditTexts in register layout. On CadastroActivity.java at method cadastrar was added the Room connection and the informations of user is registered

--- app/src/main/java/com/example/appplanteumaarvore/CadastroActivity.java
+++ app/src/main/java/com/example/appplanteumaarvore/CadastroActivity.java
@@ -1,20 +1,50 @@
 package com.example.appplanteumaarvore;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    EditText editTextNome, editTextEndereco, editTextEmail, editTextSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        editTextNome = findViewById(R.id.cadastroNome);
+        editTextEndereco = findViewById(R.id.cadastroEndereco);
+        editTextEmail = findViewById(R.id.cadastroEmail);
+        editTextSenha = findViewById(R.id.cadastroSenha);
     }
 
     public void cadastrar(View view) {
+        String nomeDigitado = editTextNome.getText().toString();
+        String enderecoDigitado = editTextEndereco.getText().toString();
+        String emailDigitado = editTextEmail.getText().toString();
+        String senhaDigitado = editTextSenha.getText().toString();
+
+        BancoUsuario bu = Room.databaseBuilder(this, BancoUsuario.class,
+                "bd-app").allowMainThreadQueries().build();
+
+        Usuario usuario = new Usuario();
+        usuario.setNome(nomeDigitado);
+        usuario.setEndereco(enderecoDigitado);
+        usuario.setEmail(emailDigitado);
+        usuario.setSenha(senhaDigitado);
+
+        UsuarioDao uDao = bu.criarBanco();
+        uDao.cadastrarUsuario(usuario);
+
+        Toast.makeText(this, "Usuário cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+
+        bu.close();
+
         Intent intent = new Intent(this, SegundaActivity.class);
         startActivity(intent);
     }
